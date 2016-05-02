aabce19372480d32d4eb50b1f3f7f628c8cef3c9
Johannes Stein <johannesstein@freeze-dev.com>
2016-05-02 15:17:11 +0200

    Add replace method

--- src/doc/index.js
+++ src/doc/index.js
@@ -4,5 +4,6 @@ import insert from './insert';
 import merge from './merge';
 import remove from './remove';
 import update from './update';
+import replace from './replace';
 
-export default { filter, find, insert, merge, remove, update };
+export default { filter, find, insert, merge, remove, update, replace };
