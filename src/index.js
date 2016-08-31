6583c98fc5c33cc583b6f94d6ae115e98f5a9989
Johannes Stein <johannesstein@freeze-dev.com>
2016-09-01 00:43:00 +0100

    Don't pass through `connect` directly

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import { connect } from 'rethinkdb';
+import r from 'rethinkdb';
 
 import database from './database';
 import table from './table';
 import doc from './doc';
+
+const connect = (...args) => r.connect.apply(r, args);
 
 export { database, table, doc, connect };
